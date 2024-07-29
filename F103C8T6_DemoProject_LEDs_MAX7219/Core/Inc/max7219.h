c06583d08105b9f0914caa855582eef70a8e34bd
Mario Niehren <mario.niehren@tonmeister.de>
2024-07-29 17:44:55 +0200

    Refactoring

--- F103C8T6_DemoProject_LEDs_MAX7219/Core/Inc/max7219.h
+++ F103C8T6_DemoProject_LEDs_MAX7219/Core/Inc/max7219.h
@@ -4,13 +4,15 @@
  *                  amount of LEDs without much effort.
  *
  * # How to use:
- * @todo
+ * 1. Initialize SPI, Load Pin and IC
+ * 2. Initialize each LED used on IC
+ * 3. Use Control functions to turn LEDs on an off or adjust settings of the IC
  *
- * # Hardware requirements:
- * @todo
+ * @note            Multiple IC use is not supported by software yet! Max7219
+ *                  supports cascading multiple ICs on one SPI Line.
  *
  * # Links:
- * @anchor          DataSheetMAX7219 [Data Sheet MAX7219](@todoaddLinkHere)
+ * @anchor          DataSheetMAX7219 [Data Sheet MAX7219](https://www.analog.com/media/en/technical-documentation/data-sheets/MAX7219-MAX7221.pdf)
  *
  * @defgroup        MAX7219_Header  Header
  * @brief           Study this part for a quick overview of this module.
@@ -30,187 +32,152 @@
 #ifndef INC_MAX7219_H_
 #define INC_MAX7219_H_
 
+#define MAX7219_NUM_DIGITS  8
 
 #include "stm32f1xx_hal.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include "timer.h"
 
-/*
- * BEGIN: Typedefs for Registers.
+/***************************************************************************//**
+ * @name      Structures and Enumerations
+ * @brief     use these structure to declare on object for each used IC and
+ *            for each LED.
+ * @{
+ ******************************************************************************/
+
+/**
+ * @brief     LED structure
  */
-
-/* Register Address [D11...D8].*/
-typedef enum
-{
-	MAX7219_REGISTER_ADRRESS_NoOp = 0x00,
-	MAX7219_REGISTER_ADDRESS_Digit0 = 0x01,
-	MAX7219_REGISTER_ADDRESS_Digit1 = 0x02,
-	MAX7219_REGISTER_ADDRESS_Digit2 = 0x03,
-	MAX7219_REGISTER_ADDRESS_Digit3 = 0x04,
-	MAX7219_REGISTER_ADDRESS_Digit4 = 0x05,
-	MAX7219_REGISTER_ADDRESS_Digit5 = 0x06,
-	MAX7219_REGISTER_ADDRESS_Digit6 = 0x07,
-	MAX7219_REGISTER_ADDRESS_Digit7 = 0x08,
-	MAX7219_REGISTER_ADDRESS_DecodeMode = 0x09,
-	MAX7219_REGISTER_ADDRESS_Intensity = 0x0A,
-	MAX7219_REGISTER_ADDRESS_ScanLimit = 0x0B,
-	MAX7219_REGISTER_ADDRESS_Shutdown = 0x0C,
-	MAX7219_REGISTER_ADDRESS_DisplayTest = 0x0F
-}MAX7219_registerAddressTypeDef;
-
-/* Shutdown Register [D7...D0] */
-typedef enum
-{
-	MAX7219_SHUTDOWN_REGISTER_shutdownMode = 0x00,
-	MAX7219_SHUTDOWN_REGISTER_normalOperation = 0x01
-}MAX7219_shutdownRegisterTypeDef;
-
-/* Decode Mode Register [D7...D0] */
-typedef enum
-{
-	MAX7219_DECODE_MODE_REGISTER_NoDecode = 0x00,
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit0 = (0x01 << 0),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit1 = (0x01 << 1),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit2 = (0x01 << 2),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit3 = (0x01 << 3),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit4 = (0x01 << 4),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit5 = (0x01 << 5),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit6 = (0x01 << 6),
-	MAX7219_DECODE_MODE_REGISTER_CodeBDigit7 = (0x01 << 7),
-	MAX7219_DECODE_MODE_REGISTER_CodeB = 0xFF
-}MAX7219_decodeModeRegisterTypeDef;
-
-/* Code B Font [D3...D0]. SEG DP [D7] is not affected by code B*/
-typedef enum
-{
-	MAX7219_CODE_B_FONT_REGISTER_0 = 0x00,
-	MAX7219_CODE_B_FONT_REGISTER_1 = 0x01,
-	MAX7219_CODE_B_FONT_REGISTER_2 = 0x02,
-	MAX7219_CODE_B_FONT_REGISTER_3 = 0x03,
-	MAX7219_CODE_B_FONT_REGISTER_4 = 0x04,
-	MAX7219_CODE_B_FONT_REGISTER_5 = 0x05,
-	MAX7219_CODE_B_FONT_REGISTER_6 = 0x06,
-	MAX7219_CODE_B_FONT_REGISTER_7 = 0x07,
-	MAX7219_CODE_B_FONT_REGISTER_8 = 0x08,
-	MAX7219_CODE_B_FONT_REGISTER_9 = 0x09,
-	MAX7219_CODE_B_FONT_REGISTER_hyphen = 0x0A,
-	MAX7219_CODE_B_FONT_REGISTER_E = 0x0B,
-	MAX7219_CODE_B_FONT_REGISTER_H = 0x0C,
-	MAX7219_CODE_B_FONT_REGISTER_L = 0x0D,
-	MAX7219_CODE_B_FONT_REGISTER_P = 0x0E,
-	MAX7219_CODE_B_FONT_REGISTER_blank = 0x0F
-}MAX7219_codeBFontRegisterTypeDef;
-
-/* No Decode Data Bits [D7...D0]*/
-typedef enum
-{
-	MAX7219_NO_DECODE_REGISTER_G = (0x01 << 0),
-	MAX7219_NO_DECODE_REGISTER_F = (0x01 << 1),
-	MAX7219_NO_DECODE_REGISTER_E = (0x01 << 2),
-	MAX7219_NO_DECODE_REGISTER_D = (0x01 << 3),
-	MAX7219_NO_DECODE_REGISTER_C = (0x01 << 4),
-	MAX7219_NO_DECODE_REGISTER_B = (0x01 << 5),
-	MAX7219_NO_DECODE_REGISTER_A = (0x01 << 6),
-	MAX7219_NO_DECODE_REGISTER_DP = (0x01 << 7)
-}MAX7219_noDecodeRegisterTypeDef;
-
-/* Scan-Limit Register [D7...D0] */
-typedef enum
-{
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0 = 0x00,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to1 = 0x01,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to2 = 0x02,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to3 = 0x03,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to4 = 0x04,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to5 = 0x05,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to6 = 0x06,
-	MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to7 = 0x07
-}MAX7219_scanLimitRegisterTypeDef;
-
-/* Display Test Register [D0] */
-typedef enum
-{
-	MAX7219_DISPLAY_TEST_REGISTER_normalOperation = 0x00,
-	MAX7219_DISPLAY_TEST_REGISTER_DisplayTestMode = 0x01
-}MAX7219_displayTestRegisterTypeDef;
-
-/* Intensity Register [D3...D0]*/
-typedef enum
-{
-	MAX7219_INTENSITY_REGISTER_0 = 0x00,
-	MAX7219_INTENSITY_REGISTER_1 = 0x01,
-	MAX7219_INTENSITY_REGISTER_2 = 0x02,
-	MAX7219_INTENSITY_REGISTER_3 = 0x03,
-	MAX7219_INTENSITY_REGISTER_4 = 0x04,
-	MAX7219_INTENSITY_REGISTER_5 = 0x05,
-	MAX7219_INTENSITY_REGISTER_6 = 0x06,
-	MAX7219_INTENSITY_REGISTER_7 = 0x07,
-	MAX7219_INTENSITY_REGISTER_8 = 0x08,
-	MAX7219_INTENSITY_REGISTER_9 = 0x09,
-	MAX7219_INTENSITY_REGISTER_10 = 0x0A,
-	MAX7219_INTENSITY_REGISTER_11 = 0x0B,
-	MAX7219_INTENSITY_REGISTER_12 = 0x0C,
-	MAX7219_INTENSITY_REGISTER_13 = 0x0D,
-	MAX7219_INTENSITY_REGISTER_14 = 0x0E,
-	MAX7219_INTENSITY_REGISTER_15 = 0x0F
-
-}MAX7219_intensityRegisterTypedef;
-/*
- * END: Typedefs for Registers.
- */
-
-
 typedef struct {
-  MAX7219_registerAddressTypeDef digit;
-  MAX7219_noDecodeRegisterTypeDef segment;
-}max7219led_TypeDef;
+  uint8_t Digit;
+  uint8_t Segment;
+}max7219_LED_TypeDef;
 
+/**
+ * @brief     max7219 data structure
+ */
 typedef struct{
-  max7219led_TypeDef led;
-  SPI_HandleTypeDef* hspi;
-  uint16_t loadPin;
-  GPIO_TypeDef* loadGPIO;
-  bool forceOn;
-  bool forceOff;
-  uint8_t segmentRegisterState[8];
+  SPI_HandleTypeDef* hspi;    /**< pointer to HAL-SPI handle */
+  uint16_t loadPin;           /**< load Pin (CS) */
+  GPIO_TypeDef* loadGPIO;     /**< load port (CS)*/
+  uint8_t SegmentRegisterState[MAX7219_NUM_DIGITS]; /**< Array of LED Register
+                                   states for each digit */
 }max7219_TypeDef;
 
-/*
- * BEGIN: Functions to initialize max7219.
+/** @} ************************************************************************/
+/* end of name "Structures and Enumerations"
+ ******************************************************************************/
+
+
+/***************************************************************************//**
+ * @name      Initialize
+ * @brief     Use these functions to initialize the LED Driver.
+ * @{
+ ******************************************************************************/
+/**
+ * @brief     Link HAL-SPI handle to the max7219 data structure
+ * @param     max7219   pointer to the users max7219 data structure
+ * @param     hspi      pointer to the HAL-SPI handle
+ * @return    none
  */
 void max7219_init_SPI(max7219_TypeDef* max7219, SPI_HandleTypeDef* hspi);
-void max7219_init_LoadPin(max7219_TypeDef* max7219, GPIO_TypeDef* loadGPIOx, uint16_t loadPin);
+
+/**
+ * @brief     Store Load Pin to the max7219 data structure
+ * @param     max7219   pointer to the users max7219 data structure
+ * @param     GPIO      Port used for the load pin
+ * @param     Pin       number of the load pin
+ * @return    none
+ */
+void max7219_init_LoadPin(max7219_TypeDef* max7219, GPIO_TypeDef* GPIO, uint16_t Pin);
+
+/**
+ * @brief     Initialize max7219 in no decode mode.
+ * @note      The no decode mode is preferred to access each LED individually.
+ *            Max7219 supports the use of "Code B" for the use with 8 segment
+ *            digit displays but it will not be used here.
+ * @param     max7219   pointer to the users max7219 data structure
+ * @return    none
+ */
 void max7219_init_NoDecode(max7219_TypeDef* max7219);
-/*
- * END: Functions to initialize max7219.
- */
 
-/*
- * BEGIN: Functions to set and reset LEDs on max7219.
+/**
+ * @brief     Initialize the coordinate of an LED. Use this function to setup
+ *            each LED.
+ * @param     LED     pointer to the LED to be initialized
+ * @param     Digit   values from 0 to 7 are possible
+ * @param     Segment values from 0 to 7 are possible. G = 0, F = 1, E = 2,
+ *                    D = 3, C = 4, B = 5, A = 6, DP = 7
+ * @return    none
  */
-void max7219_set_Led(max7219_TypeDef* max7219, max7219led_TypeDef* led);
-void max7219_reset_Led(max7219_TypeDef* max7219, max7219led_TypeDef* led);
+void max7219_init_LED(max7219_LED_TypeDef* LED, uint8_t Digit, uint8_t Segment);
+/** @} ************************************************************************/
+/* end of name "Initialize"
+ ******************************************************************************/
 
-void max7219_write_LED_register(max7219_TypeDef* max7219, uint8_t digit, uint8_t LEDreg);
 
-void max7219_set_AllLeds(max7219_TypeDef* max7219);
-void max7219_reset_AllLeds(max7219_TypeDef* max7219);
-/*
- * END: Functions to set and reset LEDs on max7219.
- */
+/***************************************************************************//**
+ * @name      Control
+ * @brief     Use these functions to control LEDs.
+ * @{
+ ******************************************************************************/
 
-/*
- * BEGIN: Functions to write max7219 registers.
+/**
+ * @brief     Turn on an LED on the IC
+ * @param     max7219   pointer to the users max7219 data structure
+ * @param     LED       pointer to the LED to turn on
+ * @return    none
  */
-void max7219_write_DecodeRegister(max7219_TypeDef* max7219, MAX7219_decodeModeRegisterTypeDef decodeMode);
-void max7219_write_IntensityRegister(max7219_TypeDef* max7219, MAX7219_intensityRegisterTypedef intensity);
-void max7219_write_ScanLimitRegister(max7219_TypeDef* max7219, MAX7219_scanLimitRegisterTypeDef scanLimit);
-void max7219_write_ShutdownRegister(max7219_TypeDef* max7219, MAX7219_shutdownRegisterTypeDef shutdownMode);
-void max7219_write_DisplayTestRegister(max7219_TypeDef* max7219, MAX7219_displayTestRegisterTypeDef displayTestMode);
-/*
- * END: Functions to write max7219 registers.
+void max7219_turnOn_LED(max7219_TypeDef* max7219, max7219_LED_TypeDef* led);
+
+/**
+ * @brief     Turn off an LED on the IC
+ * @param     max7219   pointer to the users max7219 data structure
+ * @param     LED       pointer to the LED to turn off
+ * @return    none
  */
+void max7219_turnOff_LED(max7219_TypeDef* max7219, max7219_LED_TypeDef* led);
+
+/**
+ * @brief     Write the register state of all segments on one digit.
+ *            This can be helpful for quick and efficient LED updates.
+ * @param     max7219   pointer to the users max7219 data structure
+ * @param     Digit     number [0 - 7]
+ * @param     Register  states of the LEDs of the digit.
+ *                      | D7  | D6  | D6  | D4  | D3  | D2  | D1  | D0  |
+ *                      | --- | --- | --- | --- | --- | --- | --- | --- |
+ *                      | DP  | A   | B   | C   | D   | E   | F   | G   |
+ * @return    none
+ */
+void max7219_write_LEDStatesOnDigit(max7219_TypeDef* max7219, uint8_t Digit, uint8_t Register);
+
+/**
+ * @brief     Turn all LEDs on
+ * @param     max7219   pointer to the users max7219 data structure
+ * return     none
+ */
+void max7219_turnOn_AllLEDs(max7219_TypeDef* max7219);
+
+/**
+ * @brief     Turn all LEDs off
+ * @param     max7219   pointer to the users max7219 data structure
+ * return     none
+ */
+void max7219_turnOff_AllLEDs(max7219_TypeDef* max7219);
+
+/**
+ * @brief     Control the brightness of the LEDs on the IC. The LEDs can only
+ *            be dimmed all at once, not separate.
+ * @param     max7219     pointer to the users max7219 data structure
+ * @param     Brightness  of the IC. Possible values are from 0 - 15.
+ * @return    none
+ */
+void max7219_dim_BrightnessAllLEDs(max7219_TypeDef* max7219, uint8_t Brightness);
+
+/** @} ************************************************************************/
+/* end of name "Control"
+ ******************************************************************************/
 
 /**@}*//* end of defgroup "MAX7219_Header" */
 /**@}*//* end of defgroup "MAX7219" */
--- F103C8T6_DemoProject_LEDs_MAX7219/Core/Src/max7219.c
+++ F103C8T6_DemoProject_LEDs_MAX7219/Core/Src/max7219.c
@@ -1,141 +1,247 @@
-/*
- * max7219.c
+/***************************************************************************//**
+ * @defgroup        MAX7219_Source  Source
+ * @brief           Study this part for a details about this module.
  *
- *  Created on: Aug 11, 2023
- *      Author: Mario Niehren
- */
+ * @addtogroup      MAX7219
+ * @{
+ *
+ * @addtogroup      MAX7219_Source
+ * @{
+ *
+ * @file            max7219.c
+ *
+ * @date            Aug 11, 2023
+ * @author          Mario Niehren
+ ******************************************************************************/
 
 #include <MAX7219.h>
 
-/*
-* BEGIN: Function prototypes
-*/
-void max7219_send16BitDataInterrupt(max7219_TypeDef* max7219, uint8_t* txData);
-void max7219_send16BitDataPull(max7219_TypeDef* max7219, uint8_t txMSB, uint8_t txLSB);
-/*
-* END: Function prototypes
-*/
+/* Display Test Register [D0] */
+typedef enum
+{
+  MAX7219_DISPLAY_TEST_NO_TEST = 0x00,
+  MAX7219_DISPLAY_TEST_TEST_MODE = 0x01
+}MAX7219_displayTestRegisterTypeDef;
+/* Shutdown Register [D7...D0] */
+typedef enum
+{
+  MAX7219_SHUTDOWN_ON = 0x00,
+  MAX7219_SHUTDOWN_OFF = 0x01
+}MAX7219_shutdownRegisterTypeDef;
 
-/*
- * BEGIN: Functions to initialize max7219.
+/* Register Address [D11...D8].*/
+typedef enum
+{
+  MAX7219_ADRRESS_NO_OP = 0x00,
+  MAX7219_ADDRESS_DIGIT0 = 0x01,
+  MAX7219_ADDRESS_DIGIT1 = 0x02,
+  MAX7219_ADDRESS_DIGIT2 = 0x03,
+  MAX7219_ADDRESS_DIGIT3 = 0x04,
+  MAX7219_ADDRESS_DIGIT4 = 0x05,
+  MAX7219_ADDRESS_DIGIT5 = 0x06,
+  MAX7219_ADDRESS_DIGIT6 = 0x07,
+  MAX7219_ADDRESS_DIGIT7 = 0x08,
+  MAX7219_ADDRESS_DECODE_MODE = 0x09,
+  MAX7219_ADDRESS_INTENSITY = 0x0A,
+  MAX7219_ADDRESS_SCAN_LIMIT = 0x0B,
+  MAX7219_ADDRESS_SHUTDOWN = 0x0C,
+  MAX7219_ADDRESS_DISPLAY_TEST = 0x0F
+}MAX7219_registerAddressTypeDef;
+
+/* Decode Mode Register [D7...D0] */
+typedef enum
+{
+  MAX7219_NO_DECODE = 0x00,
+  MAX7219_CODE_B_DIGIT0 = (0x01 << 0),
+  MAX7219_CODE_B_DIGIT1 = (0x01 << 1),
+  MAX7219_CODE_B_DIGIT2 = (0x01 << 2),
+  MAX7219_CODE_B_DIGIT3 = (0x01 << 3),
+  MAX7219_CODE_B_DIGIT4 = (0x01 << 4),
+  MAX7219_CODE_B_DIGIT5 = (0x01 << 5),
+  MAX7219_CODE_B_DIGIT6 = (0x01 << 6),
+  MAX7219_CODE_B_DIGIT7 = (0x01 << 7),
+  MAX7219_CODE_B = 0xFF
+}MAX7219_decodeModeRegisterTypeDef;
+
+/* Scan-Limit Register [D7...D0] */
+typedef enum
+{
+  MAX7219_DISPLAY_DIGIT0 = 0x00,
+  MAX7219_DISPLAY_DITIG0TO1 = 0x01,
+  MAX7219_DISPLAY_DITIG0TO2 = 0x02,
+  MAX7219_DISPLAY_DITIG0TO3 = 0x03,
+  MAX7219_DISPLAY_DITIG0TO4 = 0x04,
+  MAX7219_DISPLAY_DITIG0TO5 = 0x05,
+  MAX7219_DISPLAY_DITIG0TO6 = 0x06,
+  MAX7219_DISPLAY_DITIG0TO7 = 0x07
+}MAX7219_scanLimitRegisterTypeDef;
+
+/***************************************************************************//**
+ * @name      Helper Functions
+ * @brief     these functions are used for internal module related tasks.
+ * @{
+ ******************************************************************************/
+/**
+ * @brief     SPI Send function
+ * @note      In many commands, txMSB is the command address and txLSB is the
+ *            command value.
+ *            Last 16Bits of date are latched on rising edge of load pin. So it
+ *            is not possible to send data without toggling load all 16 Bits.
+ * @param     max7219   pointer to the users max7219 data structure
+ * @param     txMSB     MSB to be send (or command address)
+ * @param     txLSB     LSB to be send (or command data)
+ * @return    none
+ *
  */
+void max7219_send16BitDataPoll(max7219_TypeDef* max7219, uint8_t txMSB, uint8_t txLSB)
+{
+  /** @internal     1. Activate CS-Pin */
+  HAL_GPIO_WritePin(max7219->loadGPIO, max7219->loadPin, GPIO_PIN_RESET);
+  /** @internal     2.  Transmit Data */
+  uint32_t Timeout = 50;
+  HAL_SPI_Transmit(max7219->hspi, &txMSB, 1, Timeout);
+  HAL_SPI_Transmit(max7219->hspi, &txLSB, 1, Timeout);
+  /** @internal     2. Deactivate CS-Pin*/
+  HAL_GPIO_WritePin(max7219->loadGPIO, max7219->loadPin, GPIO_PIN_SET);
+}
+
+/** @} ************************************************************************/
+/* end of name "Helper Functions"
+ ******************************************************************************/
+
+
+/***************************************************************************//**
+ * @name      Initialize
+ * @brief     Use these functions to initialize the LED Driver.
+ * @{
+ ******************************************************************************/
+
+/* Description in .h */
 void max7219_init_SPI(max7219_TypeDef* max7219, SPI_HandleTypeDef* hspi)
 {
 	max7219->hspi = hspi;
 }
 
-void max7219_init_LoadPin(max7219_TypeDef* max7219, GPIO_TypeDef* loadGPIOx, uint16_t loadPin)
+/* Description in .h */
+void max7219_init_LoadPin(max7219_TypeDef* max7219, GPIO_TypeDef* GPIO, uint16_t Pin)
 {
-	max7219->loadGPIO = loadGPIOx;
-	max7219->loadPin = loadPin;
+	max7219->loadGPIO = GPIO;
+	max7219->loadPin = Pin;
 }
 
+/* Description in .h */
 void max7219_init_NoDecode(max7219_TypeDef* max7219)
 {
-	max7219_write_DecodeRegister(max7219, MAX7219_DECODE_MODE_REGISTER_NoDecode);
-	max7219_write_ScanLimitRegister(max7219, MAX7219_SCANLIMIT_REGISTER_DisplayDigit0to7);
-	max7219_write_ShutdownRegister(max7219, MAX7219_SHUTDOWN_REGISTER_normalOperation);
-	max7219_write_DisplayTestRegister(max7219, MAX7219_DISPLAY_TEST_REGISTER_normalOperation);
-	max7219_write_IntensityRegister(max7219, MAX7219_INTENSITY_REGISTER_3);	/* MAX7219_INTENSITY_REGISTER from 0 - 15 */
-}
-/*
- * END: Functions to initialize max7219.
- */
-
-/*
- * BEGIN: Functions to set and reset LEDs on max7219.
- */
-void max7219_set_Led(max7219_TypeDef* max7219, max7219led_TypeDef* led)
-{
-  max7219->segmentRegisterState[(led->digit - 1)] |= led->segment;
-  max7219_send16BitDataPull(max7219, led->digit, max7219->segmentRegisterState[(led->digit - 1)]);
+  max7219_send16BitDataPoll(max7219, MAX7219_ADDRESS_DECODE_MODE, MAX7219_NO_DECODE);
+  max7219_send16BitDataPoll(max7219, MAX7219_ADDRESS_SCAN_LIMIT, MAX7219_DISPLAY_DITIG0TO7);
+  max7219_send16BitDataPoll(max7219, MAX7219_ADDRESS_SHUTDOWN, MAX7219_SHUTDOWN_OFF);
+  max7219_send16BitDataPoll(max7219, MAX7219_ADDRESS_DISPLAY_TEST, MAX7219_DISPLAY_TEST_NO_TEST);
+  uint8_t Brightness = 3;
+	max7219_dim_BrightnessAllLEDs(max7219, Brightness);
 }
 
-void max7219_reset_Led(max7219_TypeDef* max7219, max7219led_TypeDef* led)
+/* Description in .h */
+void max7219_init_LED(max7219_LED_TypeDef* LED, uint8_t digit, uint8_t segment)
 {
-  max7219->segmentRegisterState[(led->digit - 1)] &= ~led->segment;
-  max7219_send16BitDataPull(max7219, led->digit, max7219->segmentRegisterState[(led->digit - 1)]);
+  /** @internal     1.  Link digit value to LED-structure. */
+  uint8_t RegisterOffset = 1;
+  LED->Digit = digit + RegisterOffset;
+  /** @internal     2.  shift 1 left by the number of segment value to get the
+   *                    register value for the LEDs segment. */
+  LED->Segment = 0x01 << segment;
+}
+/** @} ************************************************************************/
+/* end of name "Initialize"
+ ******************************************************************************/
+
+
+/***************************************************************************//**
+ * @name      Control
+ * @brief     Use these functions to control LEDs.
+ * @{
+ ******************************************************************************/
+
+/* Description in .h */
+void max7219_turnOn_LED(max7219_TypeDef* max7219, max7219_LED_TypeDef* led)
+{
+  /** @internal     1.  Set Segment Bit of LED to the Segment Register Array */
+  uint8_t RegisterOffset = 1;
+  uint8_t Index = led->Digit - RegisterOffset;
+  max7219->SegmentRegisterState[(Index)] |= led->Segment;
+
+  /** @internal     2.  Send updated Segment Register to the IC */
+  uint8_t TxSegmentRegister = max7219->SegmentRegisterState[(Index)];
+  uint8_t Digit = led->Digit;
+  max7219_send16BitDataPoll(max7219, Digit, TxSegmentRegister);
 }
 
-void max7219_write_LED_register(max7219_TypeDef* max7219, uint8_t digit, uint8_t LEDreg)
+/* Description in .h */
+void max7219_turnOff_LED(max7219_TypeDef* max7219, max7219_LED_TypeDef* led)
 {
-  max7219_send16BitDataPull(max7219, digit, LEDreg);
+  /** @internal     1.  Reset Segment Bit of LED to the Segment Register Array */
+  uint8_t RegisterOffset = 1;
+  uint8_t Index = led->Digit - RegisterOffset;
+  max7219->SegmentRegisterState[Index] &= ~led->Segment;
+
+  /** @internal     2.  Send updated Segment Register to the IC */
+  uint8_t TxSegmentRegister = max7219->SegmentRegisterState[(Index)];
+  uint8_t Digit = led->Digit;
+  max7219_send16BitDataPoll(max7219, Digit, TxSegmentRegister);
 }
 
-void max7219_set_AllLeds(max7219_TypeDef* max7219)
+/* Description in .h */
+void max7219_write_LEDStatesOnDigit(max7219_TypeDef* max7219, uint8_t Digit, uint8_t Register)
 {
-  for(MAX7219_registerAddressTypeDef digit = MAX7219_REGISTER_ADDRESS_Digit0; digit <= MAX7219_REGISTER_ADDRESS_Digit7; digit++)
+  uint8_t RegisterOffset = 1;
+  uint8_t DigitAddress = Digit + RegisterOffset;
+  max7219_send16BitDataPoll(max7219, DigitAddress, Register);
+}
+
+/* Description in .h */
+void max7219_turnOn_AllLEDs(max7219_TypeDef* max7219)
+{
+  /** @internal     Loop through all Digits and turn all segments on */
+  for(MAX7219_registerAddressTypeDef digit = MAX7219_ADDRESS_DIGIT0; digit <= MAX7219_ADDRESS_DIGIT7; digit++)
   {
     for(uint8_t segment = 0; segment < 8; segment++)
     {
-      max7219led_TypeDef led = {
-          .digit = digit,
-          .segment = (0x01 << segment)
+      max7219_LED_TypeDef led = {
+          .Digit = digit,
+          .Segment = (0x01 << segment)
       };
-      max7219_set_Led(max7219, &led);
+
+      max7219_turnOn_LED(max7219, &led);
     }
   }
 }
 
-void max7219_reset_AllLeds(max7219_TypeDef* max7219)
+/* Description in .h */
+void max7219_turnOff_AllLEDs(max7219_TypeDef* max7219)
 {
-  for(MAX7219_registerAddressTypeDef digit = MAX7219_REGISTER_ADDRESS_Digit0; digit <= MAX7219_REGISTER_ADDRESS_Digit7; digit++)
+  /** @internal     Loop through all Digits and turn all segments off */
+  for(MAX7219_registerAddressTypeDef digit = MAX7219_ADDRESS_DIGIT0; digit <= MAX7219_ADDRESS_DIGIT7; digit++)
   {
     for(uint8_t segment = 0; segment < 8; segment++)
     {
-      max7219led_TypeDef led = {
-                .digit = digit,
-                .segment = (0x01 << segment)
+      max7219_LED_TypeDef led = {
+                .Digit = digit,
+                .Segment = (0x01 << segment)
       };
-      max7219_reset_Led(max7219, &led);
+
+      max7219_turnOff_LED(max7219, &led);
     }
   }
 }
-/*
- * END: Functions to set and reset LEDs on max7219.
- */
 
-/*
- * BEGIN: Functions to write max7219 registers.
- */
-void max7219_write_DecodeRegister(max7219_TypeDef* max7219, MAX7219_decodeModeRegisterTypeDef decodeMode)
+/* Description in .h */
+void max7219_dim_BrightnessAllLEDs(max7219_TypeDef* max7219, uint8_t Brightness)
 {
-	max7219_send16BitDataPull(max7219, MAX7219_REGISTER_ADDRESS_DecodeMode, decodeMode);
+  max7219_send16BitDataPoll(max7219, MAX7219_ADDRESS_INTENSITY, Brightness);
 }
+/** @} ************************************************************************/
+/* end of name "Control"
+ ******************************************************************************/
 
-void max7219_write_IntensityRegister(max7219_TypeDef* max7219, MAX7219_intensityRegisterTypedef intensity)
-{
-	max7219_send16BitDataPull(max7219, MAX7219_REGISTER_ADDRESS_Intensity, intensity);
-}
 
-void max7219_write_ScanLimitRegister(max7219_TypeDef* max7219, MAX7219_scanLimitRegisterTypeDef scanLimit)
-{
-	max7219_send16BitDataPull(max7219, MAX7219_REGISTER_ADDRESS_ScanLimit, scanLimit);
-}
-
-void max7219_write_ShutdownRegister(max7219_TypeDef* max7219, MAX7219_shutdownRegisterTypeDef shutdownMode)
-{
-	max7219_send16BitDataPull(max7219, MAX7219_REGISTER_ADDRESS_Shutdown, shutdownMode);
-}
-
-void max7219_write_DisplayTestRegister(max7219_TypeDef* max7219, MAX7219_displayTestRegisterTypeDef displayTestMode)
-{
-	max7219_send16BitDataPull(max7219, MAX7219_REGISTER_ADDRESS_DisplayTest, displayTestMode);
-}
-/*
- * END: Functions to write max7219 registers.
- */
-
-/*
- * BEGIN: Functions to send data to max7219.
- */
-void max7219_send16BitDataPull(max7219_TypeDef* max7219, uint8_t txMSB, uint8_t txLSB)
-{
-	HAL_GPIO_WritePin(max7219->loadGPIO, max7219->loadPin, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(max7219->hspi, &txMSB, 1, 50);
-	HAL_SPI_Transmit(max7219->hspi, &txLSB, 1, 50);
-	HAL_GPIO_WritePin(max7219->loadGPIO, max7219->loadPin, GPIO_PIN_SET);
-}
-/*
- * END: Functions to send data to max7219.
- */
+/**@}*//* end of defgroup "MAX7219_Source" */
+/**@}*//* end of defgroup "MAX7219" */
